58f41c2b94fa0a8a33abc075de9649b72db0305d
chaim1 <cohen1ch@gmail.com>
2019-01-20 23:31:37 +0200

    update

--- School-Events/src/app/add-event/add-event.component.ts
+++ School-Events/src/app/add-event/add-event.component.ts
@@ -13,31 +13,32 @@ export class AddEventComponent implements OnInit {
   arraynum: number;
   ngOnInit() {
   }
-  addevent(name: string, date: Date, duration: number, classes: string) {
-    console.log(name);
+  addevent(name: string, date: Date, duration: number, classes: number) {
+    // var element = <HTMLInputElement> document.getElementById("is3dCheckBox");
+    // console.log(element);
 
-    if (!name || !date || !duration) return;
-    if (Number.isNaN(Number(duration))) return;
-    let arrayEvents = [];
-    var array = JSON.parse(window.localStorage.getItem('events'));
-    if (array!== null) {
-      for (let i = 0; i < array.length; i++) {
-        arrayEvents.push(array[i])
-      }
-       this.arraynum = array.length+1;
-    }else{
-      this.arraynum = 1
-    }
+    // if (!name || !date || !duration) return;
+    // if (Number.isNaN(Number(duration))) return;
+    // let arrayEvents = [];
+    // var array = JSON.parse(window.localStorage.getItem('events'));
+    // if (array!== null) {
+    //   for (let i = 0; i < array.length; i++) {
+    //     arrayEvents.push(array[i])
+    //   }
+    //    this.arraynum = array.length+1;
+    // }else{
+    //   this.arraynum = 1
+    // }
     
-    const event = <EventModul>{
-      id: this.arraynum,
-      name: name,
-      date: date,
-      duration: Number(duration),
-      classes: [Classroom.A, Classroom.B,Classroom.C]
-    }
-    arrayEvents.push(event)
-    let eventi = JSON.stringify(arrayEvents)
-    localStorage.setItem('events', eventi);
+    // const event = <EventModul>{
+    //   id: this.arraynum,
+    //   name: name,
+    //   date: date,
+    //   duration: Number(duration),
+    //   classes: [Classroom.A, Classroom.B,Classroom.C]
+    // }
+    // arrayEvents.push(event)
+    // let eventi = JSON.stringify(arrayEvents)
+    // localStorage.setItem('events', eventi);
   }
 }
